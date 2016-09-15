0cc3427407f06bd9b12bbc24bbcd09cabc113380
Yuji Nakazato <nakazato@edirium.co.jp>
2016-09-15 16:19:46 +0900

    Modified ProblemViewManager to handle arbitrary types of markers.

--- jp.ac.kyushu.iarch.basefunction/src/jp/ac/kyushu/iarch/basefunction/utils/ProblemViewManager.java
+++ jp.ac.kyushu.iarch.basefunction/src/jp/ac/kyushu/iarch/basefunction/utils/ProblemViewManager.java
@@ -1,104 +1,149 @@
 package jp.ac.kyushu.iarch.basefunction.utils;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 
 public class ProblemViewManager {
-	private static final String OPTIONAL_MARKER_ID = "jp.ac.kyushu.iarch.checkplugin.OptionalMarker";
+	// Instance holder.
+	private static Map<String, ProblemViewManager> managers = new HashMap<String, ProblemViewManager>();
 
-	private static ProblemViewManager problemviewmanager = new ProblemViewManager();
-
-
-	private ProblemViewManager(){
-		//Singleton
+	public static ProblemViewManager getInstance(String type) {
+		if (managers.containsKey(type)) {
+			return managers.get(type);
+		} else {
+			ProblemViewManager manager = new ProblemViewManager(type);
+			managers.put(type, manager);
+			return manager;
+		}
 	}
-	public static ProblemViewManager getInstance(){
-		return problemviewmanager;
+
+	// Marker type
+	private String type;
+
+	private ProblemViewManager(String type) {
+		this.type = type;
 	}
 
-	public static void removeAllProblems(IProject project){
+	public void removeAllProblems(IProject project, boolean includeSubtypes) {
 		try {
-			project.deleteMarkers(IMarker.PROBLEM, true, IResource.DEPTH_INFINITE);
-			project.deleteMarkers(OPTIONAL_MARKER_ID, false, IResource.DEPTH_INFINITE);
-
+			project.deleteMarkers(type, includeSubtypes, IResource.DEPTH_INFINITE);
+		} catch (CoreException e) {
+			e.printStackTrace();
+		}
+	}
+	public void removeProblems(IResource resource, boolean includeSubtypes) {
+		try {
+			resource.deleteMarkers(type, includeSubtypes, IResource.DEPTH_ZERO);
 		} catch (CoreException e) {
 			e.printStackTrace();
 		}
 	}
 
+	public IMarker createMarker(IResource resource,
+			int severity, String message, String location) {
+		try {
+			IMarker marker = resource.createMarker(type);
+			if (marker.exists()) {
+				marker.setAttribute(IMarker.SEVERITY, severity);
+				marker.setAttribute(IMarker.MESSAGE, message);
+				marker.setAttribute(IMarker.LOCATION, location);
+				return marker;
+			}
+		} catch (CoreException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public IMarker setAttribute(IMarker marker, String attribute, Object value) {
+		if (marker != null) {
+			try {
+				marker.setAttribute(attribute, value);
+				return marker;
+			} catch (CoreException e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+	public IMarker setAttribute(IMarker marker, String attribute, int value) {
+		if (marker != null) {
+			try {
+				marker.setAttribute(attribute, value);
+				return marker;
+			} catch (CoreException e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+
+	public IMarker createErrorMarker(IResource resource, String message, String location) {
+		return createMarker(resource, IMarker.SEVERITY_ERROR, message, location);
+	}
+	public IMarker createErrorMarker(IResource resource, String message, String location, int lineNumber) {
+		return setAttribute(createMarker(resource, IMarker.SEVERITY_ERROR, message, location),
+				IMarker.LINE_NUMBER, lineNumber);
+	}
+	public IMarker createWarningMarker(IResource resource, String message, String location) {
+		return createMarker(resource, IMarker.SEVERITY_WARNING, message, location);
+	}
+	public IMarker createWarningMarker(IResource resource, String message, String location, int lineNumber) {
+		return setAttribute(createMarker(resource, IMarker.SEVERITY_WARNING, message, location),
+				IMarker.LINE_NUMBER, lineNumber);
+	}
+	public IMarker createInfoMarker(IResource resource, String message, String location) {
+		return createMarker(resource, IMarker.SEVERITY_INFO, message, location);
+	}
+	public IMarker createInfoMarker(IResource resource, String message, String location, int lineNumber) {
+		return setAttribute(createMarker(resource, IMarker.SEVERITY_INFO, message, location),
+				IMarker.LINE_NUMBER, lineNumber);
+	}
+
+	//
+	// for compatibility.
+	//
+
+	// TODO: This class should not have marker ID defined in checkplugin project.
+	private static final String OPTIONAL_MARKER_ID = "jp.ac.kyushu.iarch.checkplugin.OptionalMarker";
+
+	private static ProblemViewManager problemviewmanager = getInstance(IMarker.PROBLEM);
+
+	public static ProblemViewManager getInstance(){
+		return problemviewmanager;
+	}
+
+	public static void removeAllProblems(IProject project){
+		problemviewmanager.removeAllProblems(project, true);
+		getInstance(OPTIONAL_MARKER_ID).removeAllProblems(project, false);
+	}
+
 	public static void addError(IResource resource, String message, String location) {
-		try {
-			IMarker marker = resource.createMarker(IMarker.PROBLEM);
-			marker.setAttribute(IMarker.MESSAGE, message);
-			marker.setAttribute(IMarker.SEVERITY, IMarker.SEVERITY_ERROR);
-			marker.setAttribute(IMarker.LOCATION, location);
-			marker.setAttribute(IMarker.LINE_NUMBER,11);
-		} catch (CoreException ce) {
-			ce.printStackTrace();
-		}
+		problemviewmanager.createErrorMarker(resource, message, location, 11);
 	}
-	public static IMarker addError1(IResource resource, String message, String location,int lineNumber) {
-		try {
-			IMarker marker = resource.createMarker(IMarker.PROBLEM);
-			marker.setAttribute(IMarker.MESSAGE, message);
-			marker.setAttribute(IMarker.SEVERITY, IMarker.SEVERITY_ERROR);
-			marker.setAttribute(IMarker.LOCATION, location);
-			marker.setAttribute(IMarker.LINE_NUMBER,lineNumber);
-			return marker;
-		} catch (CoreException ce) {
-			ce.printStackTrace();
-			return null;
-		}
+	public static IMarker addError1(IResource resource, String message, String location, int lineNumber) {
+		return problemviewmanager.createErrorMarker(resource, message, location, lineNumber);
 	}
-
-
-	public static void addWarning1(IResource resource, String message, String location,int lineNumber) {
-		try {
-			IMarker marker = resource.createMarker(IMarker.PROBLEM);
-			marker.setAttribute(IMarker.MESSAGE, message);
-			marker.setAttribute(IMarker.SEVERITY, IMarker.SEVERITY_WARNING);
-			marker.setAttribute(IMarker.LOCATION, location);
-			marker.setAttribute(IMarker.LINE_NUMBER,lineNumber);
-		} catch (CoreException ce) {
-			ce.printStackTrace();
-		}
+	public static void addWarning1(IResource resource, String message, String location, int lineNumber) {
+		problemviewmanager.createWarningMarker(resource, message, location, lineNumber);
 	}
-
 	public static void addInfo(IResource resource, String message, String location) {
-		try {
-			IMarker marker = resource.createMarker(IMarker.PROBLEM);
-			marker.setAttribute(IMarker.MESSAGE, message);
-			marker.setAttribute(IMarker.SEVERITY, IMarker.SEVERITY_INFO);
-			marker.setAttribute(IMarker.LOCATION, location);
-		} catch (CoreException ce) {
-			ce.printStackTrace();
-		}
+		problemviewmanager.createInfoMarker(resource, message, location);
 	}
-
-	public static void addInfo1(IResource resource, String message, String location ,int lineNumber ) {
-		try {
-			IMarker marker = resource.createMarker(IMarker.PROBLEM);
-			marker.setAttribute(IMarker.MESSAGE, message);
-			marker.setAttribute(IMarker.SEVERITY, IMarker.SEVERITY_INFO);
-			marker.setAttribute(IMarker.LOCATION, location);
-			marker.setAttribute(IMarker.LINE_NUMBER,lineNumber);
-		} catch (CoreException ce) {
-			ce.printStackTrace();
-		}
+	public static void addInfo1(IResource resource, String message, String location, int lineNumber) {
+		problemviewmanager.createInfoMarker(resource, message, location, lineNumber);
 	}
 
 	public static void addOptionalInfo(IResource resource, String message, String location, int lineNumber) {
-		try {
-			IMarker marker = resource.createMarker(OPTIONAL_MARKER_ID);
-			if (marker.exists()) {
-				marker.setAttribute(IMarker.MESSAGE, message);
-				//marker.setAttribute(IMarker.LOCATION, location);
-				marker.setAttribute(IMarker.LINE_NUMBER,lineNumber);
-			}
-		} catch (CoreException ce) {
-			ce.printStackTrace();
-		}
+		ProblemViewManager manager = getInstance(OPTIONAL_MARKER_ID);
+		IMarker marker = manager.createInfoMarker(resource, message, location, lineNumber);
+		// delete severity and location.
+		manager.setAttribute(marker, IMarker.SEVERITY, null);
+		manager.setAttribute(marker, IMarker.LOCATION, null);
 	}
 }
